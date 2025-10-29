0deb81aaa8caae1a2bb82840d397ee5d38215f5c
HoaTran <hoatlkse183433@fpt.edu.vn>
2025-10-30 03:56:19 +0700

    fix AdminDashboardPage

--- src/pages/admin/AdminDashboardPage.tsx
+++ src/pages/admin/AdminDashboardPage.tsx
@@ -87,9 +87,9 @@ const AdminDashboardPage: React.FC = () => {
                 outerRadius={100}
                 fill="#8884d8"
                 dataKey="value"
-                label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+                label={({ name, percent }) => `${name} ${((percent as number) * 100).toFixed(0)}%`}
               >
-                {workshopData.map((entry, index) => (
+                {workshopData.map((_, index) => (
                   <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
               </Pie>
